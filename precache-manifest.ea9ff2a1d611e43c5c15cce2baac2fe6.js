d9853fccf135b37ba1156ce1524945928b014803
mohamed gassama <bouyagassama@gmail.com>
2019-10-16 15:07:56 -0400

    Updates

---
+++ precache-manifest.ea9ff2a1d611e43c5c15cce2baac2fe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49301cb529f3da1e8b80effe91c9dac",
+    "url": "/cool-pic/index.html"
+  },
+  {
+    "revision": "73089f649e65a837b1ca",
+    "url": "/cool-pic/static/css/main.060d5c1b.chunk.css"
+  },
+  {
+    "revision": "d810eac22d83ba9e8e2e",
+    "url": "/cool-pic/static/js/2.b066ebd8.chunk.js"
+  },
+  {
+    "revision": "73089f649e65a837b1ca",
+    "url": "/cool-pic/static/js/main.e7f33dd4.chunk.js"
+  },
+  {
+    "revision": "d00a186541fa9882878d",
+    "url": "/cool-pic/static/js/runtime-main.bd907349.js"
+  }
+]);
